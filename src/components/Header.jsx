853d8ac2823403d5b04f6903a9061792dd688257
federicodececco <federicodececco1@gmail.com>
2025-01-28 15:55:00 +0100

    add:header

---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+export default function Header() {
+  return (
+    <header className="container">
+      <nav className="row">
+        <div>
+          <img src="./dc-logo.png" alt="" />
+        </div>
+        <ul className="row">
+          <li>
+            <a href="">CHARACHTERS</a>
+          </li>
+          <li>
+            <a href="">COMICS</a>
+          </li>
+          <li>
+            <a href="">MOVIES</a>
+          </li>
+          <li>
+            <a href="">TV</a>
+          </li>
+          <li>
+            <a href="">GAMES</a>
+          </li>
+          <li>
+            <a href="">COLLECTIBLES</a>
+          </li>
+          <li>
+            <a href="">VIDEOS</a>
+          </li>
+          <li>
+            <a href="">FANS</a>
+          </li>
+          <li>
+            <a href="">NEWS</a>
+          </li>
+          <li>
+            <a href="">SHOP</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main() {
+  return (
+    <main>
+      <div>--CONTENT goes here--</div>
+      <div></div>
+    </main>
+  );
+}
