723ae989fd77605641f60fee1e93010305f5766c
federicodececco <federicodececco1@gmail.com>
2025-01-28 16:19:42 +0100

    add:main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,37 @@
 export default function Main() {
   return (
     <main>
-      <div>--CONTENT goes here--</div>
-      <div></div>
+      <section>
+        <div className="container">
+          <div className="top-content">--CONTENT goes here--</div>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <ul>
+            <li>
+              <img src="./buy.png" alt="" />
+              <a href=""> DIGITAL COMICS</a>
+            </li>
+            <li>
+              <img src="./buy2.png" alt="" />
+              <a href="">DC MERCHANDISE</a>
+            </li>
+            <li>
+              <img src="./buy3.png" alt="" />
+              <a href="">SUBSCRIPTION</a>
+            </li>
+            <li>
+              <img src="./buy4.png" alt="" />
+              <a href="">COMICS SHOP LOCATION</a>
+            </li>
+            <li>
+              <img src="./buy4.png" alt="" />
+              <a href="">DC POWER VISA</a>
+            </li>
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
