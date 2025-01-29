0ec548a27820cf2385556422dd60f4fe1f072746
federicodececco <federicodececco1@gmail.com>
2025-01-29 15:25:59 +0100

    add: dynamic array in footer's top section

---
+++ src/data/footerMenu.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    id: 1,
+    title: "DC COMICS",
+    links: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"],
+  },
+  {
+    id: 2,
+    title: "SHOP",
+    links: ["Shop DC", "Shop DC Collectibles"],
+  },
+  {
+    id: 3,
+    title: "DC",
+    links: [
+      "Terms Of Use",
+      "Privacy policy (New)",
+      "Ad Choices",
+      "Advertising",
+      "Jobs",
+      "Subscription",
+      "Talent Workshop",
+      "CPSC Crtificates",
+    ],
+  },
+  {
+    id: 4,
+    title: "SITES",
+    links: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"],
+  },
+];
